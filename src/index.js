fb29f8ab58dccd0b438e966c0acc1ee389eb7bac
naji sabha <nageammar628@gmail.com>
2025-02-15 20:54:21 +0200

    hasob

--- src/index.js
+++ src/index.js
@@ -5,5 +5,10 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+document.querySelectorAll('.add-to-card-btn').forEach(item => {
+    item.addEventListener('click', () => {
+        alert('Item added to card');
+    })
+});
 // باقي الكود الخاص بك
 console.log('Hello, world!');
