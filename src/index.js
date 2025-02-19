6b5c0992d73f290672df139cde4f737dff0b0abc
naji sabha <nageammar628@gmail.com>
2025-02-19 17:55:10 +0200

    message

--- src/index.js
+++ src/index.js
@@ -4,18 +4,24 @@ import './css/styles.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 // حساب سعر المنتج 
 
-document.querySelectorAll('[data-product-qunatity]').forEach(item =>{
-    item .addEventListener('change',() => {
+document.querySelectorAll('[data-product-qunatity]').forEach(item => {
+    item.addEventListener('change', () => {
         const newQuantity = item.value;
         const parent = item.closest('[data-product-info]');
         const pricePerUbit = parent.getAttribute('data-product-price');
-        const total = newQuantity * pricePerUbit ;
-        parent.querySelector('.total-price-for-product').innerHTML = total + $ ;
-
+        const total = newQuantity * pricePerUbit;
+        parent.querySelector('.total-price-for-product').innerHTML = "$" + total;
+        let total_monye = 0 ;
+        document.querySelectorAll('[data-product-info]').forEach(product => {
+            const pricePerUnite = product.getAttribute('data-product-price');
+            const qunatity = product.querySelector('[data-price-quantity]').value;
+            const totalPriceForProduct = pricePerUnite * qunatity ;
+            totalPriceForProduct = total_monye + totalPriceForProduct ;
+        })
     })
 }
 );
-
+// شيئ اخر 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 document.querySelectorAll('.add-to-card-btn').forEach(item => {
